98ad6ca340d3514fe88b270f0b584e6a8d52a8af
Mario <mpucheu126>
2021-05-18 02:05:36 -0400

    Creacion de interfaces

---
+++ codigo.produccion/Interfaces/IArchivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigo.produccion.Interfaces
+{
+    public interface IArchivo
+    {
+        string Leer(string ruta);
+        List<string> Lineas { get; set; }
+    }
+}
---
+++ codigo.produccion/Interfaces/IEquipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigo.produccion.Interfaces
+{
+    public interface IEquipo
+    {
+        string Nombre { get; }
+        List<string> Estudiantes { get; }
+        void AgregarParticipantes(string estudiante);
+        List<string> Temas { get; set; }
+    }
+}
